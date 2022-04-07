96bb36b194bf0563e5cc5ae46dd32d8fdd690cf4
nontawatJ <nontawat.mak@gmail.com>
2022-04-07 00:33:22 -0700

    test

---
+++ js/techS.js
@@ -0,0 +1,49 @@
+//deal with hover effect
+function techSkillEnterHover(skillType) {
+    document.getElementById(skillType).style.backgroundColor = "rgba(194, 192, 192, 0.849)";
+}
+function techSkillLeaveHover(skillType) {
+    document.getElementById(skillType).style.backgroundColor = "white";
+}
+//expanding techSkill
+function expTechSkill(skillType,expBtmSkillType,minBtmSkillType) {
+    document.getElementById(skillType).className = "techSkill-desc " + "expand";
+    document.getElementById(skillType).style.backgroundColor = "white";
+    document.getElementById(skillType).style.transition = "height 1s, transform 1s";
+    document.getElementById(expBtmSkillType).style.display = "none";
+    document.getElementById(minBtmSkillType).style.display = "inline";
+}
+
+
+//minimize techSkill
+function minTechSkill(skillType,expBtmSkillType,minBtmSkillType){
+    document.getElementById(skillType).className = "techSkill-desc " + skillType;
+    document.getElementById(skillType).style.backgroundColor = "white";
+    document.getElementById(expBtmSkillType).style.display = "inline";
+    document.getElementById(minBtmSkillType).style.display = "none";
+}
+ 
+
+//techSkill Langueges
+var expBtmLangueges = document.getElementById("expBtmLanguages");
+
+expBtmLangueges.addEventListener('mouseenter',function(){techSkillEnterHover("languages")});
+expBtmLangueges.addEventListener('mouseleave',function(){techSkillLeaveHover("languages")});
+expBtmLangueges.addEventListener('click',function(){expTechSkill("languages","expBtmLanguages","minBtmLanguages")});
+
+var minBtmLangueges = document.getElementById("minBtmLanguages");
+minBtmLangueges.addEventListener('mouseenter',function(){techSkillEnterHover("languages")});
+minBtmLangueges.addEventListener('mouseleave',function(){techSkillLeaveHover("languages")});
+minBtmLangueges.addEventListener('click',function(){minTechSkill("languages","expBtmLanguages","minBtmLanguages")});
+
+//techSkill Tools
+var expBtmTools = document.getElementById("expBtmTools");
+
+expBtmTools.addEventListener('mouseenter',function(){techSkillEnterHover("tools")});
+expBtmTools.addEventListener('mouseleave',function(){techSkillLeaveHover("tools")});
+expBtmTools.addEventListener('click',function(){expTechSkill("tools","expBtmTools","minBtmTools")});
+
+var minBtmTools = document.getElementById("minBtmTools");
+minBtmTools.addEventListener('mouseenter',function(){techSkillEnterHover("tools")});
+minBtmTools.addEventListener('mouseleave',function(){techSkillLeaveHover("tools")});
+minBtmTools.addEventListener('click',function(){minTechSkill("tools","expBtmTools","minBtmTools")});
